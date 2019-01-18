e3e7235dbd210eeedf5046142f84d86d10e1342a
kensystem <kenmel28@gmail.com>
2019-01-18 11:18:40 -0500

    Primer Commit, estructura del PY

---
+++ empresa/src/test/java/com/tropical/empresa/EmpresaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tropical.empresa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmpresaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
